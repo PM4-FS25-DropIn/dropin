125a47475a2d61770c2253bfa5fa2779f5e4c3ba
Leo <leo.boedi@proton.me>
2025-03-16 17:12:22 +0100

    Initial iOS Project setup

---
+++ iOS/dropin/dropinTests/dropinTests.swift
@@ -0,0 +1,17 @@
+//
+//  dropinTests.swift
+//  dropinTests
+//
+//  Created by leo on 16.03.2025.
+//
+
+import Testing
+@testable import dropin
+
+struct dropinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
