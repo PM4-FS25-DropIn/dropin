c763969d683cb8b2e256062c58de6f569196e266
ShpetimV <shpetimveseli77@gmail.com>
2025-04-01 20:41:54 +0200

    Moved Files

---
+++ client/ios/dropin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dropin
+//
+//  Created by leo on 16.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showOnboarding = true
+
+    var body: some View {
+        if showOnboarding {
+            OnboardingView(showOnboarding: $showOnboarding)
+        } else {
+            RegistrationView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ client/ios/dropin/dropinApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropinApp.swift
+//  dropin
+//
+//  Created by leo on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct dropinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
