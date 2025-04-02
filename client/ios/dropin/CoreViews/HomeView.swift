7a2a4dffecf9dc300e8dc3f5a8c970603bf3b6cb
boedileo <boedileo@students.zhaw.ch>
2025-04-02 15:05:41 +0200

    Implemented first version of homeview

--- client/ios/dropin/CoreViews/HomeView.swift
+++ client/ios/dropin/CoreViews/HomeView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedCategory: EventCategory = .forYou
+    @State private var events: [DropInEvent] = [dummyEvent]
+    
     var body: some View {
-        Text("This is the home view")
+        VStack {
+            HStack {
+                Spacer()
+                Image(systemName: "bell.fill")
+                    .padding(.horizontal)
+                    .foregroundStyle(.secondary)
+            }
+            FriendAvatarCarousel()
+            EventCategoryTabView(selectedCategory: $selectedCategory)
+                .padding(.horizontal)
+                .padding(.vertical, 5)
+            
+            ScrollView {
+                LazyVStack(alignment: .center, spacing: 25) {
+                    ForEach(events.indices, id: \.self) { index in
+                        EventCard(event: events[index])
+                    }
+                }
+                .padding()
+            }
+            .scrollIndicators(.hidden)
+            .refreshable {
+                // Refresh and fetch new data from db
+            }
+            .task {
+                // Initial fetch from db
+            }
+        }
     }
 }
 
