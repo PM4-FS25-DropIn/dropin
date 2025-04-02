cfc0bff7d26807d448eaed19b38718953c8a3c90
boedileo <boedileo@students.zhaw.ch>
2025-04-02 14:51:01 +0200

    Added a file for models

---
+++ client/ios/dropin/Models.swift
@@ -0,0 +1,18 @@
+//
+//  Models.swift
+//  dropin
+//
+//  Created by leo on 02.04.2025.
+//
+
+import Foundation
+
+
+enum EventCategory: String, CaseIterable {
+    case forYou = "For You"
+    case trending = "Trending"
+    case nearby = "Nearby"
+    case startingSoon = "Starting Soon"
+    case ongoing = "Ongoing"
+    case sponsored = "Sponsored"
+}
