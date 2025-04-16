b098b3eebb46268a786c83e4707d42a2c1d99eb5
x-tr4ce <moritz@feuchter.email>
2025-04-16 21:36:43 +0200

    auto format
    better readability

--- client/ios/dropin/DummyEvent.swift
+++ client/ios/dropin/DummyEvent.swift
@@ -7,4 +7,21 @@
 
 import Foundation
 
-let dummyEvent = DropInEvent(id: 1, createdAt: Date(), title: "Event", description: "Some event description", imagePaths: ["default.event.thumbnail"], userId: UUID(), start: Date(), end: Date(), latitude: 20, longitude: 29, maxSlots: 20, takenSlots: 5, visibility: .public, ageRestricted: false, chatEnabled: true, status: .upcoming)
+let dummyEvent = DropInEvent(
+    id: 1,
+    createdAt: Date(),
+    title: "Event",
+    description: "Some event description",
+    imagePaths: ["default.event.thumbnail"],
+    userId: UUID(),
+    start: Date(),
+    end: Date(),
+    latitude: 20,
+    longitude: 29,
+    maxSlots: 20,
+    takenSlots: 5,
+    visibility: .public,
+    ageRestricted: false,
+    chatEnabled: true,
+    status: .upcoming
+)
