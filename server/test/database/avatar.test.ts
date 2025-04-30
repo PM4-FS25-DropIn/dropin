0f9fb4adf061a20cf16555913cd09ee65ba3411a
Tobias <160762353+mormeli@users.noreply.github.com>
2025-04-30 23:21:43 +0200

    add more tests

--- server/test/database/avatar.test.ts
+++ server/test/database/avatar.test.ts
@@ -3,16 +3,22 @@ import assert from 'node:assert';
 import { createClientHelper, createUser, generateRandomString } from './dbhelpers.js';
 import { SupabaseClient, User } from '@supabase/supabase-js';
 
+/**
+ * Tests if an authenticated user can upload an avatar into their directory.
+ */
 test('Users can upload avatars into their directory', async (t) => {
   const { client, user } = await setupRandomClientAndLogin();
 
   const uploadResult = await client.storage.from('avatars')
     .upload(user.identities![0]!.id + '/avatar1.png', generateRandomBlob(1024));
 
-  assert.ifError(uploadResult.error);
-  console.log("Upload result:", uploadResult.data);
+  assert.equal(uploadResult.error, null, "Expected upload to succeed, but it failed: " + uploadResult.error?.message);
+  assert.notEqual(uploadResult.data, null, "Expected data not to be null, but got null.");
 });
 
+/**
+ * A user should not be able to upload an avatar into another user's directory.
+ */
 test('Uploaded avatar needs to be in user directory', async (t) => {
   const { client } = await setupRandomClientAndLogin();
 
@@ -21,9 +27,11 @@ test('Uploaded avatar needs to be in user directory', async (t) => {
 
   assert.equal(uploadResult.data, null, "Expected data to be null, but got: " + uploadResult.data);
   assert.notEqual(uploadResult.error, null);
-  console.log("Upload successfuly failed with: " + uploadResult.error!.message);
 });
 
+/**
+ * An user should not be able to upload an avatar into the root directory.
+ */
 test('Limit avatar upload to 5MB', async (t) => {
   const { client, user } = await setupRandomClientAndLogin();
   
@@ -37,6 +45,9 @@ test('Limit avatar upload to 5MB', async (t) => {
 
 });
 
+/**
+ * A user should not be able to upload more than 10 avatars.
+ */
 test('Only allow up to 10 avatar images per user', async (t) => {
   const { client, user } = await setupRandomClientAndLogin();
 
@@ -50,6 +61,9 @@ test('Only allow up to 10 avatar images per user', async (t) => {
   assert.equal(lastResult!.error, null, "Expected upload to fail, but got: " + lastResult!.error?.message);
 });
 
+/**
+ * A user should not be able to upload an avatar with a non-image file type.
+ */
 test('Disallow non-image files', async (t) => {
   const { client, user } = await setupRandomClientAndLogin();
   
@@ -58,6 +72,88 @@ test('Disallow non-image files', async (t) => {
   
     assert.equal(uploadResult.data, null, "Expected data to be null, but got: " + uploadResult.data);
     assert.notEqual(uploadResult.error, null);
+});
+
+/**
+ * A user should be able to delete their own avatar.
+ */
+test('User can delete their own avatar', async (t) => {
+  const { client, user } = await setupRandomClientAndLogin();
+
+  const targetFile = user.identities![0]!.id + '/avatar1.png';
+  const uploadResult = await client.storage.from('avatars')
+      .upload(targetFile, generateRandomBlob(1024));
+
+  assert.equal(uploadResult.error, null, "Expected the upload to succeed, but it failed: " + uploadResult.error?.message);
+
+  const deleteResult = await client.storage.from('avatars')
+      .remove([targetFile]);
+
+  assert.equal(deleteResult.error, null, "Expected delete action to succed, but it failed " + deleteResult.error?.message);
+});
+
+/**
+ * A user should not be able to delete another user's avatar.
+ */
+test('Disallow deleting other users avatars', async (t) => {
+  const { client: clientA, user: userA } = await setupRandomClientAndLogin();
+  
+  const targetFile = userA.identities![0]!.id + '/avatar1.png';
+  const uploadResult = await clientA.storage.from('avatars')
+    .upload(targetFile, generateRandomBlob(1024));
+
+  assert.ifError(uploadResult.error);
+  
+  const { client: clientB } = await setupRandomClientAndLogin();
+
+  const deleteResult = await clientB.storage.from('avatars')
+    .remove([targetFile]);
+  
+  assert.notEqual(deleteResult.data, null, "Expected data not to be null, but got null.");
+  // data.length should be 0, because the file was not deleted
+  assert.equal(deleteResult.data?.length, 0, "Expected data length to be 0, but got: " + deleteResult.data?.length);
+});
+
+/**
+ * A authenticated user should be able to get another user's avatar.
+ */
+test('Authenticated user can get other users avatars', async (t) => {
+  const { client: clientA, user: userA } = await setupRandomClientAndLogin();
+  
+  const targetFile = userA.identities![0]!.id + '/avatar1.png';
+  const uploadResult = await clientA.storage.from('avatars')
+    .upload(targetFile, generateRandomBlob(1024));
+
+  assert.equal(uploadResult.error, null, "Expected upload to succeed, but it failed: " + uploadResult.error?.message);
+  
+  const { client: clientB } = await setupRandomClientAndLogin();
+
+  const downloadResult = await clientB.storage.from('avatars')
+    .download(targetFile);
+  
+  assert.equal(downloadResult.error, null, "Expected download action to succeed, but it failed: " + downloadResult.error?.message);
+  assert.notEqual(downloadResult.data, null, "Expected data not to be null, but got null.");
+  assert.equal(downloadResult.data?.size, 1024, "Expected data size to be 1024, but got: " + downloadResult.data?.size);
+});
+
+/**
+ * An unauthenticated user should not be able to get another user's avatar.
+ */
+test('An unaithenticated user should not be able to get another users avatar', async (t) => {
+  const { client: uploadClient, user: uploadUser } = await setupRandomClientAndLogin();
+
+  const targetFile = uploadUser.identities![0]!.id + '/avatar1.png';
+  const uploadResult = await uploadClient.storage.from('avatars')
+    .upload(targetFile, generateRandomBlob(1024));
+
+  assert.equal(uploadResult.error, null, "Expected upload to succeed, but it failed: " + uploadResult.error?.message);
+
+  const client = createClientHelper();
+  
+  const downloadResult = await client.storage.from('avatars')
+    .download(targetFile);
+
+  assert.notEqual(downloadResult.error, null, "Expected download action to fail, but it succeeded: " + downloadResult.error?.message);
 });
 
 async function setupRandomClientAndLogin(): Promise<{client: SupabaseClient<any, any, any>, user: User}> {
