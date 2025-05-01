6b5c97d0323294654c7c3ad9aa0e19147afb67ae
x-tr4ce <moritz@feuchter.email>
2025-05-01 11:27:08 +0200

    adapted carussel to new Avatar

--- client/ios/dropin/Components/FriendAvatarCarousel.swift
+++ client/ios/dropin/Components/FriendAvatarCarousel.swift
@@ -8,13 +8,11 @@
 import SwiftUI
 
 struct FriendAvatarCarousel: View {
-    var imageURLs: [URL] = [URL(filePath: "")!]// just for testing, should later be used with actual fetched profiles from the database
-    
     var body: some View {
         ScrollView(.horizontal) {
             HStack(spacing: 15) {
                 ForEach(1...10, id: \.self) { _ in
-                    FriendAvatar(imageURL: URL(filePath: "")!)
+                    FriendAvatar()
                 }
             }
             .padding()
