573c3b2fe68984396cee1022862909adcfcb1403
Leo <leo.boedi@proton.me>
2025-05-06 15:03:04 +0200

    Renamed project

--- client/ios/pm4-dropin/Supabase.swift
+++ client/ios/pm4-dropin/Supabase.swift
@@ -9,8 +9,13 @@ import Foundation
 import OSLog
 import Supabase
 
-let supabaseURL = ProcessInfo.processInfo.environment["SUPABASE_URL"] ?? ""
-let supabaseKey = ProcessInfo.processInfo.environment["SUPABASE_KEY"] ?? ""
+// Details for production supabase
+//let supabaseURL = ProcessInfo.processInfo.environment["SUPABASE_URL"] ?? ""
+//let supabaseKey = ProcessInfo.processInfo.environment["SUPABASE_KEY"] ?? ""
+
+// Details for local supabase
+let supabaseURL = "http://127.0.0.1:54321"
+let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
 
 let supabase = SupabaseClient(
     supabaseURL: URL(string: supabaseURL)!,
