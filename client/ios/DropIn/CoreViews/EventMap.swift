85a2f9f3718f3fe047ad1a08edb8bd12c4d81996
Leo <leo.boedi@proton.me>
2025-05-08 20:07:29 +0200

    Adjusted eventmap to use wizard

--- client/ios/DropIn/CoreViews/EventMap.swift
+++ client/ios/DropIn/CoreViews/EventMap.swift
@@ -57,10 +57,7 @@ struct EventMap: View {
             .sheet(isPresented: $showCreateViewSheet) {
                 pinLocation = nil
             } content: {
-                NavigationStack {
-                    EventCreateView(defaultEvent: DropInEvent(title: "", description: "", imagePaths: ["default.event.thumbnail"], start: Date(), end: Date(), latitude: pinLocation?.latitude ?? 0, longitude: pinLocation?.longitude ?? 0, slotLimit: 2, ageRestricted: false, chatEnabled: true))
-                        .toolbar(.hidden)
-                }
+                CreateEventWizard()
             }
             .onChange(of: selectedItem) {
                 guard let selectedItem else { return }
