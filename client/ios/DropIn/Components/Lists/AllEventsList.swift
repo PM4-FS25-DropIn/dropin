0dedf844e9e5e58983f95961c2f13abc92f540b3
Leo <leo.boedi@proton.me>
2025-05-13 17:55:37 +0200

    Lists bug fixes

--- client/ios/DropIn/Components/Lists/AllEventsList.swift
+++ client/ios/DropIn/Components/Lists/AllEventsList.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 
-/// Displays a list of joined events of user (excluding the ones created by user!)
+/// Displays a list of joined events of user
 struct AllEventsList: View {
     @Environment(EventStore.self) private var eventStore
     @Environment(AuthService.self) private var authService
@@ -20,7 +20,7 @@ struct AllEventsList: View {
     var body: some View {
         List {
             ForEach(eventStore.joinedEvents) { event in
-                EventRowItem(event: event, showHostBadge: event.userId == authService.userId)
+                EventRowItem(event: event, isHost: event.userId == authService.userId)
                     .listRowInsets(EdgeInsets())
                     .listRowSeparator(.hidden)
                     .swipeActions(edge: .trailing) {
