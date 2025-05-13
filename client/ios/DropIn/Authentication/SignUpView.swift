d83ef05ed9d7ec13b64776585cb3e0e754086128
Leo <leo.boedi@proton.me>
2025-05-13 17:57:18 +0200

    Added simple form validation in forms

--- client/ios/DropIn/Authentication/SignUpView.swift
+++ client/ios/DropIn/Authentication/SignUpView.swift
@@ -55,6 +55,8 @@ struct SignUpView: View {
             AuthTextField("Email", value: $authData.email)
             AuthSecureField("Password", value: $authData.password)
             AuthSecureField("Confirm Password", value: $authData.confirmPassword)
+                
+            displayFormError()
             Button {
                 signUp()
             } label: {
@@ -62,12 +64,49 @@ struct SignUpView: View {
                     .frame(maxWidth: .infinity)
             }
             .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.roundedRectangle(radius: 15))
+            .buttonBorderShape(.roundedRectangle(radius: 30))
             .controlSize(.large)
             .bold()
             .padding(.top, 40)
-            .disabled(signUpState.isRunning)
+            .disabled(signUpState.isRunning || !isFormValid())
         }
+    }
+    
+    private func isFormValid() -> Bool {
+        if authData.username.isEmpty || authData.email.isEmpty || authData.password.isEmpty || authData.confirmPassword.isEmpty  {
+            return false
+        } else if authData.password != authData.confirmPassword {
+            return false
+        } else if authData.password.count < 6 {
+            return false
+        }
+        return true
+    }
+    
+    private func displayFormError() -> some View {
+        var show = true
+        var message = ""
+        if authData.username.isEmpty {
+            message = "Username is empty."
+        } else if authData.email.isEmpty {
+            message = "Email is empty."
+        } else if authData.password.isEmpty {
+            message = "Password is empty."
+        } else if authData.confirmPassword.isEmpty {
+            message = "Please confirm password."
+        } else if authData.password != authData.confirmPassword {
+            message = "Passwords don't match."
+        } else if authData.password.count < 6 {
+            message = "Password should be at least 6 characters."
+        } else {
+            show = false
+        }
+        
+        return Text(message)
+            .font(.footnote)
+            .bold()
+            .foregroundStyle(.red)
+            .opacity(show ? 1 : 0)
     }
     
     private func signUp() {
