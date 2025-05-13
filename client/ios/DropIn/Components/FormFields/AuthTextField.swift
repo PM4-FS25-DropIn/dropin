8945685c4a29fb107a32c102bb59a80ee741e680
Leo <leo.boedi@proton.me>
2025-05-13 17:56:04 +0200

    Fixed form fields

--- client/ios/DropIn/Components/FormFields/AuthTextField.swift
+++ client/ios/DropIn/Components/FormFields/AuthTextField.swift
@@ -2,20 +2,20 @@ import SwiftUI
 
 struct AuthTextField: View {
     let placeholder: String
-    var value: Binding<String>
+    @Binding var value: String
     
     init(_ placeholder: String, value: Binding<String>) {
         self.placeholder = placeholder
-        self.value = value
+        self._value = value
     }
     
     init(value: Binding<String>) {
         self.placeholder = ""
-        self.value = value
+        self._value = value
     }
     
     var body: some View {
-        TextField(placeholder, text: value)
+        TextField(placeholder, text: $value)
             .padding()
             .padding(.horizontal, 15.0)
             .overlay(RoundedRectangle(cornerRadius: 30)
