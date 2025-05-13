e859a2d86e0fcc81634c683fc04f4b17cd663f33
Leo <leo.boedi@proton.me>
2025-05-13 17:55:07 +0200

    Reworked EventDetailView
    Features Edit Button, minimap, overhauled design and bug fixes

--- client/ios/DropIn/CoreViews/EventDetailView.swift
+++ client/ios/DropIn/CoreViews/EventDetailView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import MapKit
 
 struct EventDetailView: View {
     @Environment(EventStore.self) private var eventStore
@@ -7,28 +8,91 @@ struct EventDetailView: View {
     @State private var attendanceStatus: AttendanceStatus = .undetermined
     
     var event: DropInEvent
+    var isHost: Bool = true
     
     var body: some View {
         ScrollView {
-            TabView {
-                ForEach(event.imagePaths, id: \.self) { imagePath in
-                    image(imagePath: imagePath)
-                }
+            imageCarousel
+            VStack(alignment: .center, spacing: 20) {
+                titleAndDescription
+                DropInButton(attendanceStatus: $attendanceStatus, action: joinEvent, extended: true)
+                EventQuickInfo(event: event)
+                minimap
             }
-            .tabViewStyle(.page)
-            .containerRelativeFrame(.vertical, count: 12, span: 5, spacing: 0)
+            .padding()
+        }
+        .toolbar(.hidden)
+        .scrollIndicators(.hidden)
+        .onAppear {
+            attendanceStatus = eventStore.checkIfEventIsJoinedByUser(event) ? .joined : .undetermined
+        }
+       
+    }
+    
+    // MARK: - Image Carousel
+    
+    private var imageCarousel: some View {
+        TabView {
+            ForEach(event.imagePaths, id: \.self) { imagePath in
+                image(imagePath: imagePath)
+            }
+        }
+        .tabViewStyle(.page)
+        .containerRelativeFrame(.vertical, count: 12, span: 5, spacing: 0)
+        .overlay(alignment: .topTrailing) {
+            if isHost {
+                NavigationLink("Edit") {
+                    EventEditView(event: event)
+                }
+                .font(.footnote)
+                .bold()
+                .foregroundStyle(.white)
+                .padding(10)
+                .background(.accent)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .padding()
+            }
+        }
+    }
+    
+    // MARK: - Title and Description
+    
+    private var titleAndDescription: some View {
+        VStack(alignment: .center) {
+            if isHost {
+                Image(systemName: "crown.fill")
+                    .foregroundStyle(.orange)
+                    .font(.caption)
+            }
             Text(event.title)
                 .font(.title)
                 .bold()
             Text(event.description)
-                .font(.subheadline)
-            buttonGroup
-            EventQuickInfo(event: event)
-                .padding()
-            Spacer()
+                .font(.caption)
+                .bold()
+                .foregroundStyle(.secondary)
         }
-        .onAppear {
-            attendanceStatus = eventStore.checkIfEventIsJoinedByUser(event) ? .joined : .undetermined
+    }
+    
+    // MARK: - Minimap
+    
+    private var minimap: some View {
+        VStack {
+            Text("Location")
+                .font(.title2)
+                .bold()
+            Text(formatCoordinates(latitude: event.latitude, longitude: event.longitude))
+                .font(.caption)
+                .bold()
+                .foregroundStyle(.secondary)
+            Map(initialPosition: .region(MKCoordinateRegion(center: .init(latitude: event.latitude, longitude: event.longitude), span: .init(latitudeDelta: 0.001, longitudeDelta: 0.001)))) {
+                Marker("DropIn", systemImage: "drop", coordinate: CLLocationCoordinate2D(latitude: event.latitude, longitude: event.longitude))
+                    .tint(.indigo)
+            }
+            .disabled(true)
+            .containerRelativeFrame(.vertical, count: 12, span: 4, spacing: 0)
+            .mapControlVisibility(.hidden)
+            .clipShape(RoundedRectangle(cornerRadius: 30))
         }
     }
     
@@ -47,16 +111,13 @@ struct EventDetailView: View {
         }
     }
     
-    private var buttonGroup: some View {
-        DropInButton(attendanceStatus: $attendanceStatus, action: joinEvent, extended: true)
-            .padding()
-    }
+    // MARK: - Join Event Function
     
     private func joinEvent() {
         Task {
             joinEventTaskStatus = .running
             do {
-                try await eventStore.joinEvent(event)
+                _ = try await eventStore.joinEvent(event)
                 joinEventTaskStatus = .success
                 attendanceStatus = .joined
             } catch {
