153cf6c59c79f60c984bcdf3c0ea209149d91a7e
Leo <leo.boedi@proton.me>
2025-05-23 19:51:16 +0200

    Logo updates and keys

--- client/ios/DropIn/Supabase.swift
+++ client/ios/DropIn/Supabase.swift
@@ -11,21 +11,21 @@ import Supabase
 
 private var getSupabaseURL: String {
     #if DEBUG
-    return ProcessInfo.processInfo.environment["LOCAL_SUPABASE_URL"] ?? ""
+    return "http://127.0.0.1:54321"
     #elseif TEST
-    return ProcessInfo.processInfo.environment["TEST_SUPABASE_URL"] ?? ""
+    return "https://pgdgktaphxujfnduwzqb.supabase.co"
     #else
-    return "http://160.85.252.162:8080"
+    return "https://owjhkqoogpvwddoazsto.supabase.co"
     #endif
 }
 
 private var getSupabaseKey: String {
     #if DEBUG
-    return ProcessInfo.processInfo.environment["LOCAL_SUPABASE_KEY"] ?? ""
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
     #elseif TEST
-    return ProcessInfo.processInfo.environment["TEST_SUPABASE_KEY"] ?? ""
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnZGdrdGFwaHh1amZuZHV3enFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MzIzMjgsImV4cCI6MjA2MzAwODMyOH0.NpsWa-c2nZhD6BOnRYYhzVcoSMN35WVN9V5JM7OwuA0"
     #else
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzQ0MTQ5NjAwLAogICJleHAiOiAxOTAxOTE2MDAwCn0.8vzFDrqMsw3ebkPVpk4CbwOGccnj5mQ7eXGZ4hVkXkU"
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93amhrcW9vZ3B2d2Rkb2F6c3RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTc4NjQsImV4cCI6MjA2MzU5Mzg2NH0.J8faEbnGt432YOeAFXBWoQzXDEXcf9Clb1INvhg8PUY"
     #endif
 }
 
