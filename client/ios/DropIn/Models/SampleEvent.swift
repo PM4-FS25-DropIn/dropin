88e5d4b577cf41824932a564f1491aa00f285a19
Leo <leo.boedi@proton.me>
2025-05-24 15:00:38 +0200

    Added url image for sampleEvent

--- client/ios/DropIn/Models/SampleEvent.swift
+++ client/ios/DropIn/Models/SampleEvent.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-let sampleEvent: DropInEvent = .init(id: 1, title: "Event", description: "Event Description", imagePaths: ["default.event.thumbnail"], userId: UUID(), start: .now, end: .now, slotLimit: 5, ageRestricted: true, chatEnabled: true, location: .init(type: "Point", coordinates: [0,0]))
-
+let sampleEvent: DropInEvent = .init(id: 1, title: "Event", description: "Event Description", imagePaths: ["https://canto-wp-media.s3.amazonaws.com/app/uploads/2019/08/19194138/image-url-3.jpg"], userId: UUID(), start: .now, end: .now, slotLimit: 5, ageRestricted: true, chatEnabled: true, location: .init(type: "Point", coordinates: [0,0]))
 
 
 
