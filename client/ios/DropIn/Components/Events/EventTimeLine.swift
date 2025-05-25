3a17f88d3ee292cf6c782e164ce19e0b3c5a77b2
Leo <leo.boedi@proton.me>
2025-05-26 00:25:48 +0200

    TimeLine better layout

--- client/ios/DropIn/Components/Events/EventTimeLine.swift
+++ client/ios/DropIn/Components/Events/EventTimeLine.swift
@@ -24,6 +24,7 @@ struct EventTimeLine: View {
                 placeholder
             } else {
                 banner
+                Spacer()
                 eventBubbleStack
             }
         }
