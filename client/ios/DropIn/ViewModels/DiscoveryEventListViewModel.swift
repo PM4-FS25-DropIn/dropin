d3b5f507cda9bc4a00c7d2a0d75507c32bbfe40c
Leo <leo.boedi@proton.me>
2025-05-26 14:56:25 +0200

    Migration updates

--- client/ios/DropIn/ViewModels/DiscoveryEventListViewModel.swift
+++ client/ios/DropIn/ViewModels/DiscoveryEventListViewModel.swift
@@ -19,19 +19,21 @@ final class DiscoveryEventListViewModel {
     
     func updateEvents() {
         guard let eventStore else { return }
-        events = eventStore.getNotJoinedEvents()
+        events = removePastEvents(eventStore.getNotJoinedEvents())
     }
     
     func refreshFeed() async throws {
         guard let eventStore else { return }
         try await eventStore.clearEvents()
-        events = try await eventStore.loadMoreNearbyEvents()
+        _ = try await eventStore.loadMoreNearbyEvents()
+        events = removePastEvents(eventStore.getNotJoinedEvents())
     }
     
     func fetchAdditionalEvents() async throws {
         guard let eventStore else { return }
-        let additionalEvents = try await eventStore.loadMoreNearbyEvents()
-        events.append(contentsOf: additionalEvents)
+        
+        _ = try await eventStore.loadMoreNearbyEvents()
+        events = removePastEvents(eventStore.getNotJoinedEvents())
     }
     
     func joinEvent(_ event: DropInEvent) async throws {
@@ -56,6 +58,12 @@ final class DiscoveryEventListViewModel {
         
         return events.filter { event in
             event.start > now && event.start <= in30Minutes
+        }
+    }
+    
+    private func removePastEvents(_ events: [DropInEvent]) -> [DropInEvent] {
+        return events.filter { event in
+            return event.end > Date()
         }
     }
     
