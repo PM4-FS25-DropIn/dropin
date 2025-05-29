723d156a81bd622b689a5846f1b57c4e2893095b
Tobias <160762353+mormeli@users.noreply.github.com>
2025-05-30 00:04:23 +0200

    remove unused policies

--- client/supabase/migrations/20250421203700_limit_avatar_file_type_size_amount.sql
+++ client/supabase/migrations/20250421203700_limit_avatar_file_type_size_amount.sql
@@ -20,28 +20,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
--- Basic RLS policy to allow authenticated users to upload, update and delete avatar images
-
-CREATE POLICY "Authenticated users can upload an avatar"
-    ON storage.objects
-    FOR INSERT TO authenticated WITH CHECK (
-        (bucket_id = 'avatars'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])
-    );
-
-CREATE POLICY "Allow owner files to delete their own avatars"
-    ON storage.objects
-    FOR DELETE TO authenticated USING (
-        (bucket_id = 'avatars'::text) 
-        AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])
-    );
-
-CREATE POLICY "Allow owner files to update their own avatars"
-    ON storage.objects
-    FOR DELETE TO authenticated USING (
-        (bucket_id = 'avatars'::text) 
-        AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])
-    );
-
 -- Limit amout of avatar images to 10 per user
 
 CREATE POLICY "Only allow up to 10 avatar images per user"
