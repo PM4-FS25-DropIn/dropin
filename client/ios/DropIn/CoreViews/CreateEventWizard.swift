f3937047c78b7321875dcf5cae30b078afc78130
Leo <leo.boedi@proton.me>
2025-06-01 01:48:35 +0200

    Trying to fix mini map

--- client/ios/DropIn/CoreViews/CreateEventWizard.swift
+++ client/ios/DropIn/CoreViews/CreateEventWizard.swift
@@ -99,9 +99,9 @@ struct CreateEventWizard: View {
                         .tint(.indigo)
                 }
                 .mapControlVisibility(.hidden)
-                .containerRelativeFrame(.vertical, count: 10, span: 5, spacing: 0)
+                .containerRelativeFrame(.vertical, count: 10, span: 6, spacing: 0)
                 .gesture(MyLongPressGesture { position in
-                    if let loc = proxy.convert(position, from: .global) {
+                    if let loc = proxy.convert(position, from: .local) {
                         vm.pinLocation = loc
                         vm.event.location.coordinates[0] = loc.longitude
                         vm.event.location.coordinates[1] = loc.latitude
