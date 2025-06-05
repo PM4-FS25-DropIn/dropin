b8f49d31dcda6a47b0ba23b9986ddbd8af6c9990
Tobias <160762353+mormeli@users.noreply.github.com>
2025-06-05 11:41:49 +0200

    remove upload limit

--- client/supabase/migrations/20250421203700_limit_avatar_file_type_size_amount.sql
+++ client/supabase/migrations/20250421203700_limit_avatar_file_type_size_amount.sql
@@ -1,30 +1,10 @@
 -- ===========================================
--- Migration:     Limit avatar file type, size, type and amount
+-- Migration:     Limit avatar file type, size and type
 --
 -- Description:
 --   - Limits the file size of avatar images to 5MB
 --   - Only allows png, jpeg, webp, gifs and apng for avatar images
---   - Only allows up to 10 avatar images per user
 -- ============================================
 
-DROP POLICY IF EXISTS "Authenticated users can upload an avatar" on storage.objects;
-
-CREATE OR REPLACE FUNCTION public.count_user_uploads_in(bucket text)
-RETURNS INTEGER AS $$
-BEGIN
-  RETURN (
-    SELECT COUNT(*) FROM storage.objects
-    WHERE (bucket_id = bucket)
-      AND (owner = auth.uid())
-  );
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
-
--- Limit amout of avatar images to 10 per user
-
-CREATE POLICY "Only allow up to 10 avatar images per user"
-    ON storage.objects AS RESTRICTIVE
-    FOR INSERT TO authenticated WITH CHECK (
-        (bucket_id = 'avatars')
-        AND (count_user_uploads_in('avatars') <= 10)
-    );
+-- Limit file size to 10MB and limit mime types to images only
+UPDATE storage.buckets SET file_size_limit = 10485760, allowed_mime_types = ARRAY['image/*'] WHERE id = 'avatars';
